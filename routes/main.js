2ea0d1794982cc94924afb4c3a7ebb51a43c8131
SyikoXynあ <87143163+Fxc4Sanz@users.noreply.github.com>
2022-02-04 00:52:29 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -3,8 +3,8 @@ __path = process.cwd()
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => {
-    res.sendFile(__path + '/dashboard.html')
+router.get('/docs', (req, res) => {
+	res.sendFile(__path + '/dashboard.html')
 })
 
 router.get('/config', (req, res) => {
